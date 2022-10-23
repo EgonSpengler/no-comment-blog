6c570a8a17bff0ca75de858d35e883bcffc8dc6d
Tyler Adam <adam.tyler.j@gmail.com>
2022-10-23 01:32:31 -0400

    Synchronous scripts fix for Vercel

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html >
-      <script src="/path/to/highlight.min.js"></script>
-      <script>hljs.highlightAll();</script>
+      <Script src="/path/to/highlight.min.js"></Script>
+      <Script>hljs.highlightAll();</Script>
       <Head />
       <body className="bg-grey-800 text-yellow-100">
         <Main />
