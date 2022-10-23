d9cfb29246d0f0b898b0e7b6a036ba4ac4a2417f
Tyler Adam <adam.tyler.j@gmail.com>
2022-10-23 01:47:15 -0400

    Yet more Vercel fixes

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html >
-      <Script src="/path/to/highlight.min.js"></Script>
-      <Script>hljs.highlightAll();</Script>
       <Head />
       <body className="bg-grey-800 text-yellow-100">
         <Main />
