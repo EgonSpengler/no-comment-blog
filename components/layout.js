740cbd8ac5d6799512d82c8ee3102da3e345d426
Tyler Adam <adam.tyler.j@gmail.com>
2022-10-25 22:34:15 -0400

    Third post and additional layout/theme fixes

--- components/layout.js
+++ components/layout.js
@@ -3,18 +3,17 @@ import Link from 'next/link';
 export default function Layout({ children }) {
   return (
     <div className='flex flex-col min-h-screen text-yellow-100'>
-      <header className='sticky inset-x-0 top-0 bg-grey-900 pt-4 pb-2'>
-        <div className='grid grid-cols-5 mx-auto justify-center'>
-          <div></div>
-          <Link href='/'><a className='pt-10 justify-self-end'>🏡</a></Link>
-          <div>
-            <div className='mx-auto text-yellow-200 font-mono text-3xl text-center'> &#47;* no comment &#47;* </div>
-            <div className='mx-auto text-yellow-300 font-mono text-l text-center py-2'> The Loosely Logical Linux Library </div>
+      <header className='top-0 bg-grey-900 pt-4 pb-2'>
+        <div className='parent'>
+          <Link href='/'><a className='sm:max-md:absolute left lg:text-right lg:pt-12 lg:pr-16'>🏡</a></Link>
+          <div className="center">
+            <div className='mx-auto text-yellow-200 font-mono text-2xl md:text-4xl text-center'> &#47;* no comment *&#47; </div>
+            <div className='mx-auto text-yellow-300 font-mono text-md md:text-xl text-center py-2'> The Loosely Logical Linux Library </div>
           </div>
-          <div></div>
+          <div className="right"></div>
         </div>
       </header>
-      <main className='container mx-auto flex-1 pt-6'>{children}</main>
+      <main className='container mx-auto flex-1 pt-6 px-2'>{children}</main>
       <footer className='sticky inset-x-0 bottom-0 bg-grey-900 mt-8 py-4'>
         <div className='container mx-auto flex justify-center text-yellow-300 font-mono text-l'>
           &copy; 2022 Tyler Adam
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -10,7 +10,7 @@ var md = require('markdown-it')({
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return '<pre class="hljs overflow-x-visible"><code>' +
+        return '<pre class="hljs border border-grey-700 border-2 text-xs lg:text-sm overflow-x-auto scrollbar scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-teal-600 scrollbar-track-teal-800"><code>' +
                hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
                '</code></pre>';
       } catch (__) {
@@ -18,11 +18,12 @@ var md = require('markdown-it')({
       }
     }
 
-    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    return '<pre class="hljs border border-grey-700 border-2 text-xs lg:text-sm overflow-x-auto scrollbar scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-teal-600 scrollbar-track-teal-800"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
   },
   linkify: true
 });
 md.linkify.set({ fuzzyEmail: false });  // disables converting email to link
+
 
 // Currently default but I want to apply a better style to inline code
 md.renderer.rules.code_inline = function (tokens, idx, options, env, slf) { 
@@ -30,7 +31,6 @@ md.renderer.rules.code_inline = function (tokens, idx, options, env, slf) {
 
   return  '<code' + slf.renderAttrs(token) + '>' + md.utils.escapeHtml(token.content) + '</code>';
 };
-
 
 
 export async function getStaticPaths() {
@@ -61,7 +61,13 @@ export default function PostPage({ frontmatter, content }) {
   return (
     <div className='prose max-w-4xl mx-auto prose-theme'>
       <h1>{frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: md.render(content) }} />
+      <div dangerouslySetInnerHTML={{ __html: md.render(content)
+          .replaceAll("/prompt/", "<span class='hljs-bullet'>egon</span>@<span class='hljs-formula'>odin</span>:<span class='hljs-string'>")
+          .replaceAll("/dir/", "</span>$")
+          .replaceAll("/cursor", "<span class='cursor'>█</span>")
+          .replaceAll("/blu/", "<span class='hljs-string'>")
+          .replaceAll("/grn", "<span class='hljs-name'>")
+          .replaceAll("/end/", "</span>") }} />
     </div>
   );
 }
